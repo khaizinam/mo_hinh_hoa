6621a01b899d8858d0f796fed3f5002694027c84
khaizinam <khai.nguyenhuu@hcmut.edu.vn>
2022-08-31 09:18:15 +0700

    a

--- ex-1.py
+++ ex-1.py
@@ -7,7 +7,7 @@ pi = math.pi
 a = 2
 b = 4
 c = -2
-d = 2
+d = -2
 R0 = 5/4
 J0 = 5/4
 def delta(a,b,c,d):
@@ -18,18 +18,15 @@ def lamda(a,b,c,d):
         deltaSqrt = math.sqrt(deltaValue)
         lamda1 = (a + b - deltaSqrt) / 2
         lamda2 = (a + b + deltaSqrt) / 2
-        print("delta > 0 : lamda 1: "+str(lamda1)+", lamda2 : "+str(lamda2))
         return [lamda1 , lamda2]
     elif deltaValue == 0 :
         x = ( a + d ) / 2
-        print("delta = 0 : lamda 1: "+str(lamda1)+", lamda2 : "+str(lamda2))
         return x
     else :
         deltaValue = abs(deltaValue)
         deltaSqrt = math.sqrt(deltaValue)
         a1 = ( a + d ) / 2
         b1 = deltaSqrt / 2
-        print("delta < 0 : lamda: "+str(a1)+"+-"+str(b1)+"i")
         return [ a1, b1]
 def Fxy(a,b,c,d,R0,J0 ,k1,k2):
     a1 = 1 ; b1 = 1 ; c1 = R0
@@ -37,7 +34,6 @@ def Fxy(a,b,c,d,R0,J0 ,k1,k2):
     D = a1 * b2 - a2 * b1
     Dx = c1 * b2 - c2 * b1
     Dy = a1 * c2 - a2 * c1
-    print("C 1: "+str(Dx / D)+", C : "+str(Dy / D))
     return [Dx / D, Dy / D]
 def Fxy2(a,b,c,d,R0,J0 ,k):
     a1 = 1 ; b1 = 0 ; c1 = R0
@@ -45,12 +41,10 @@ def Fxy2(a,b,c,d,R0,J0 ,k):
     D = a1 * b2 - a2 * b1
     Dx = c1 * b2 - c2 * b1
     Dy = a1 * c2 - a2 * c1
-    print("C 1: "+str(Dx / D)+", C : "+str(Dy / D))
     return [Dx / D, Dy / D]
 def Fxy3(a,b, R0,J0 , m, n):
     c1 = R0
     c2 = ((b * J0) - ((m - a ) * R0)) / n
-    print("C 1: "+str(c1)+", C : "+str(c2))
     return [c1, c2]
 def fR(t,a,b,c,d,R0,J0):
     deltaValue = delta(a,b,c,d)
@@ -59,6 +53,11 @@ def fR(t,a,b,c,d,R0,J0):
         k2 = lamda(a,b,c,d)[1]
         c1 = Fxy( a, b, c, d, R0, J0, k1, k2 )[0]
         c2 = Fxy( a, b, c, d, R0, J0, k1, k2 )[1]
+        print("delta > 0 ")
+        print("lamda 1 : "+str(k1))
+        print("lamda 2 : "+str(k1))
+        print("C 1 : "+str(c1))
+        print("C 2 : "+str(c2))
         R = c1 * math.exp(k1 * t) + c2 * math.exp(k2 * t) 
         # print("k1 = " + str(k1)) 
         # print("k2 = " + str(k2)) 
@@ -69,6 +68,10 @@ def fR(t,a,b,c,d,R0,J0):
         k = lamda(a,b,c,d)
         c1 = R0
         c2 = Fxy( a, b, c, d, R0, J0, k)[1]
+        print("delta = 0 ")
+        print("lamda : "+str(k1))
+        print("C 1 : "+str(c1))
+        print("C 2 : "+str(c2))
         R = c1 * math.exp(k * t) + c2 * math.exp(k * t) 
         # print("k1 = k2 = " + str(k)) 
         # print("c1 = " + str(c1)) 
@@ -79,6 +82,11 @@ def fR(t,a,b,c,d,R0,J0):
         n = lamda(a,b,c,d)[1]
         c1 = Fxy3(a,b, R0,J0 , m, n)[0]
         c2 = Fxy3(a,b, R0,J0 , m, n)[1]
+        print("delta < 0 ")
+        print("lamda 1: "+str(m) + " + " +str(n)+"i")
+        print("lamda 2: "+str(m) + " - " +str(n)+"i")
+        print("C 1 : "+str(c1))
+        print("C 2 : "+str(c2))
         R = math.exp(m * t) * (c1 * math.cos((n * t)) + c2 * math.sin((n * t)))
         # print("k1 = " + str(m) + " +/- " + str(n)+"i") 
         # print("c1 = " + str(c1)) 
