7f2f28dcb182022e1a77168144d778585d26f3ca
hoaminzy <48762609+student1060632fpt@users.noreply.github.com>
2022-09-04 14:20:32 +0700

    fix label in portrait

--- portrait.py
+++ portrait.py
@@ -37,21 +37,28 @@ Q = plt.quiver(Y1, Y2, u, v, color='g')
 tspan = np.linspace(0 , 10 ,200, endpoint=False)
 for elementSt, St in enumerate(Sts):
     sol = odeint(ivpSys, St, tspan,args=(a, b, c, d))
+    print("ele = "+str(elementSt)+", sts = "+str(Sts))
     if elementSt == 0:
         color = "r"
     else :
-        color = "b"
-    plt.plot(sol[:,0], sol[:,1], color)
+        color = "darkgray"
+    plt.plot(sol[:,0], sol[:,1], color, label='Trajectory')
     plt.plot([sol[-1,0]], [sol[-1,0]], 'g',linewidth=5.0) # end
 #--
 x = np.linspace(-4.5, 4.5, 100)
+
+#nullcline
 def fx(x,a,b):
     return -a * x / b
-plt.plot(x,fx(x,a,b))
-plt.plot(x,fx(x,c,d))
+plt.plot(x,fx(x,a,b),linestyle='dashed',linewidth=0.75,color='royalblue',label='Nullcline 1')
+plt.plot(x,fx(x,c,d),linestyle='dashed',linewidth=0.75,color='magenta',label='Nullcline 2')
+
+# plot fixed points
+plt.plot(0, 0,"red", marker = "o", markersize = 10.0, color='grey', label='Fixed point')
+
 plt.xlabel("Romeo's love for Juliet")
 plt.ylabel("Juliet's love for Romeo")
-plt.legend(["Vector field","Trajectory", "Trajectory 2", "Trajectory 3", "Nullcline 1", "Nullcline 2"])
+plt.legend()
 plt.xlim([-4.5, 4.5])
 plt.ylim([-4.5, 4.5])
 plt.show()
